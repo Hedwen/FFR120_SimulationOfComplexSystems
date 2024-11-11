7d17e5176e7f0c7874d03e4746a4ea76188e6ac9
Hedwen <hedvigwe@chalmers.se>
2024-11-11 19:01:43 +0100

    Created the new assignments

--- HW1/Brownian_Disk_with_Lennard_Jones_Potential.py
+++ HW1/Brownian_Disk_with_Lennard_Jones_Potential.py
@@ -8,8 +8,8 @@ import math
 def InitializeParticles(X_disk, Y_disk, sigma, N_part, v_part, x_min, x_max, y_min, y_max):
     # Initialize particles positions
     x0, y0 = np.meshgrid(
-        np.linspace(x_min, x_max, N_part),
-        np.linspace(y_min, y_max, N_part))
+        np.linspace(x_min, x_max, int(np.sqrt(N_part))),
+        np.linspace(y_min, y_max, int(np.sqrt(N_part))))
     x0 = x0.flatten()
     y0 = y0.flatten()
 
@@ -32,12 +32,29 @@ def InitializeParticles(X_disk, Y_disk, sigma, N_part, v_part, x_min, x_max, y_m
 
     return x0, y0, velocities
 
+def CheckBoxConstraint(x, y, vx, vy, x_min, x_max, y_min, y_max):
+    if x < x_min:
+        vx = -vx
+        x = 2*x_min - x
+    elif x > x_max:
+        vx = -vx
+        x = 2*x_max - x
+
+    if y < y_min:
+        vy = -vy
+        y = 2*y_min - y
+    elif y > y_max:
+        vy = -vy
+        y = 2*y_max - y
+    
+    return x, y, vx, vy
+
 #%% Parameters
 
 # Particle Parameters
 m_part = 1
 v_part = 10
-N_part = 25
+N_part = 625
 
 # Disk Parameters
 m_disk = 10
@@ -67,12 +84,14 @@ N_t = int(T_tot/delta_t)
 diskPosition = np.zeros((2,N_t))
 
 for n_t in range(N_t):
-    print(f'Starting measurement {n_t}')
+    if n_t%1000 == 0:
+        print(f'Starting measurement {n_t}')
+
     diskPosition[:,n_t] = [X_disk, Y_disk]
 
     dx, dy = X_disk - x0, Y_disk - y0
 
-    r = np.sqrt((dx)**2 + (dy)**2) - R_disk
+    r = np.sqrt(dx**2 + dy**2) - R_disk
 
     # Calculate forces
     Fx_disk, Fy_disk = 0, 0
@@ -80,18 +99,20 @@ for n_t in range(N_t):
         if r[i] > 0:
             F = 24 * epsilon/r[i] * (2*(sigma/r[i])**12 - (sigma/r[i])**6)
 
-            Fx, Fy = F * dx[i] / (r[i] + R_disk), F * dy[i] / (r[i] + R_disk)
+            Fx, Fy = F * dx[i] / r[i], F * dy[i] / r[i]
 
             Fx_disk += Fx
             Fy_disk += Fy
 
             # Update particle velocities
-            velocities[0,i] += Fx / m_part * delta_t
-            velocities[1,i] += Fy / m_part * delta_t
+            velocities[0,i] += -Fx / m_part * delta_t
+            velocities[1,i] += -Fy / m_part * delta_t
+        else:
+            print('It happened')
 
     # Update disk velocity
-    v_disk[0] += np.sum(Fx_disk) / m_disk * delta_t
-    v_disk[1] += np.sum(Fy_disk) / m_disk * delta_t
+    v_disk[0] += Fx_disk / m_disk * delta_t
+    v_disk[1] += Fy_disk / m_disk * delta_t
 
     # Update particle positions
     x0 += velocities[0,:] * delta_t
@@ -99,20 +120,16 @@ for n_t in range(N_t):
 
     # Check box constraints
     for i in range(len(x0)):
-        if x0[i] < x_min or x0[i] > x_max:
-            velocities[0,i] = -velocities[0,i]
-        if y0[i] < y_min or y0[i] > y_max:
-            velocities[1,i] = -velocities[1,i]
+        x0[i], y0[i], velocities[0,i], velocities[1,i] = CheckBoxConstraint(x0[i], y0[i], velocities[0,i], velocities[1,i], 
+                                                                            x_min, x_max, y_min, y_max)
 
     # Update disk position
     X_disk += v_disk[0] * delta_t
     Y_disk += v_disk[1] * delta_t
 
     # Check box constraints
-    if X_disk < x_min+R_disk or X_disk > x_max-R_disk:
-        v_disk[0] = -v_disk[0]
-    if Y_disk < y_min+R_disk or Y_disk > y_max-R_disk:
-        v_disk[1] = -v_disk[1]
+    X_disk, Y_disk, v_disk[0], v_disk[1] = CheckBoxConstraint(X_disk, Y_disk, v_disk[0], v_disk[1],
+                                                              x_min+R_disk, x_max-R_disk, y_min+R_disk, y_max-R_disk)
 
 # Calculate MSD
 MSD = np.zeros((N_t))
@@ -139,4 +156,6 @@ plt.title("Mean Square Displacement (MSD) of Disk")
 plt.grid()
 plt.show()
 
-# %%
+# %% Calculate estimate of D
+
+MSD(tau) = 4*D*tau
